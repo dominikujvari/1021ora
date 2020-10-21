a52727c5e7b5c4a39a4e4ea2ebc3e3cfee5f013f
Ujvári Dominik <domi152001@outlook.com>
2020-10-21 11:13:28 +0200

    kotelezo

--- 1021ora/Program.cs
+++ 1021ora/Program.cs
@@ -8,8 +8,17 @@ namespace _1021ora
 {
     class Program
     {
+        static Random rnd = new Random();
         static void Main(string[] args)
         {
+            var t = new int[10];
+            for (int i = 0; i < t.Length; i++)
+            {
+                t[i] = rnd.Next(10, 100);
+                Console.WriteLine($"{t[i]},");
+
+            }
+            Console.ReadKey();
         }
     }
 }
